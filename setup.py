cd2855440ca50b893644265bd0ed569f8fdb3e1a
jyotim2devops <87702858+jyotim2devops@users.noreply.github.com>
2025-11-15 15:10:17 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Jyoti Mishra",
+    author_email="jyotiranjan.mishra2020@gmail.com",
+    packages=find_packages()
+)
